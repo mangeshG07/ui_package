9d0830116f3c0b7eacb57c59b2785e17bf6ca3d6
mangeshG07 <mangeshg@deepmindsit.com>
2026-06-15 15:12:09 +0530

    testing

--- lib/src/test/core/test_wrapper.dart
+++ lib/src/test/core/test_wrapper.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ui_package/ui_package.dart';
 
 class TestWrapper {
   static Widget wrap(Widget child) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: child),
+    return ScreenUtilConfig.init(
+      context: Get.context!,
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: child),
+      ),
+      minTextAdapt: true,
     );
   }
 }
--- lib/src/test/widget/widget_tester_helper.dart
+++ lib/src/test/widget/widget_tester_helper.dart
@@ -3,17 +3,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 class WidgetTesterHelper {
   static Future<void> enterText(
-      WidgetTester tester,
-      String key,
-      String value,
-      ) async {
+    WidgetTester tester,
+    String key,
+    String value,
+  ) async {
     await tester.enterText(find.byKey(Key(key)), value);
   }
 
-  static Future<void> tap(
-      WidgetTester tester,
-      String key,
-      ) async {
+  static Future<void> tap(WidgetTester tester, String key) async {
     await tester.tap(find.byKey(Key(key)));
     await tester.pumpAndSettle();
   }
